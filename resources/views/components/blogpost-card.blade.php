0b40b54ac41dd8658d341b82fc424a7a0bfb14cc
Afshan01 <afshanalamengg@gmail.com>
2024-01-30 20:18:35 +0530

    added blogpost-card component

---
+++ resources/views/components/blogpost-card.blade.php
@@ -0,0 +1,17 @@
+<x-card class="mb-4 mr-4 ml-4">
+    <div class="mb-4 flex justify-between">
+        <h2 class="text-lg font-medium">{{ $blogpost->title }}</h2>
+    </div>
+    <div class="mb-4 justify-between">
+        <div class="text-sm text-dark-500">Blog Published on: {{ $blogpost->created_at->format('d-m-Y') }}</div>
+    </div>
+    <div class="mb-4 justify-between">
+        <h2 class="text-sm font-medium">Author: Author Name
+            {{-- {{ $blogpost->title }} --}}
+        </h2>
+    </div>
+
+    <p class="text-sm text-slate-500">{!!  nl2br($blogpost->content)  !!}</p>
+
+    {{ $slot }}
+</x-card>
