5774a49141ff114cffadedbf0a308617d4850449
Afshan01 <afshanalamengg@gmail.com>
2024-01-30 20:20:38 +0530

    modified index.blade.php in blogposts

--- resources/views/blogposts/index.blade.php
+++ resources/views/blogposts/index.blade.php
@@ -1,20 +1,12 @@
 <x-layout>
     @foreach ($blogposts as $blogpost)
-        <x-card class="mb-4 mr-4 ml-4">
-            <div class="mb-4 flex justify-between">
-                <h2 class="text-lg font-medium">{{ $blogpost->title }}</h2>
+        <x-blogpost-card class="mb-4" :$blogpost>
+            <div class="mt-4">
+                <x-link-button :href="route('blogposts.show', $blogpost) ">
+                    View More
+                </x-link-button>
             </div>
-            <div class="mb-4 justify-between">
-                <div class="text-sm text-dark-500">Blog Published on: {{ $blogpost->created_at->format('d-m-Y') }}</div>
-            </div>
-            <div class="mb-4 justify-between">
-                <h2 class="text-sm font-medium">Author: Author Name
-                    {{-- {{ $blogpost->title }} --}}
-                </h2>
-            </div>
-
-            <p class="text-sm text-slate-500">{!!  nl2br($blogpost->content)  !!}</p>
-        </x-card>
+        </x-blogpost-card>
     @endforeach
 
 </x-layout>
