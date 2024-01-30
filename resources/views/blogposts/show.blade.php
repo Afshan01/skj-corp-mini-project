4edc5d29d82621245b9a5c45bdc5341df357c738
Afshan01 <afshanalamengg@gmail.com>
2024-01-30 20:31:47 +0530

    modified show.blade.php in blogposts view

--- resources/views/blogposts/show.blade.php
+++ resources/views/blogposts/show.blade.php
@@ -1,3 +1,16 @@
 <x-layout>
+    <nav class="mb-4 ml-4 mr-4">
+        <ul class="flex space-x-4 text-slate-500">
+           <li>
+              <a href="/">Home</a>
+           </li>
+           <li>-></li>
+           <li>
+              <a href="{{ route('blogposts.index') }}">
+                 All BlogPosts
+              </a>
+           </li>
+        </ul>
+     </nav>
     <x-blogpost-card :$blogpost/> 
 </x-layout>
