648d3afdd0352bf01255b76aaf87a78334111e2b
Afshan01 <afshanalamengg@gmail.com>
2024-01-30 23:36:37 +0530

    added support for alpine js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,9 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>SKJ CORP BLOGS</title>
-        @vite('resources/css/app.css')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
+    <body class="from-10% via-30% to-90% mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-200 via-sky-200 to-emerald-200 text-slate-700">        
         <div class="text-4xl mb-4 mr-4 ml-4 text-center">SKJ Corp Blogs</div>
         {{ $slot }}
     </body>
